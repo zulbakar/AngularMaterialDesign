d8bf5bd6b9a005648672f02bcd0c0f3542230722
azizulBakar <azizul_sb@yahoo.com>
2017-07-04 17:08:52 +0800

    update profile page

---
+++ app/scripts/directives/amd-profile-form.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name angularMaterialBoilerplateApp.directive:amdProfileForm
+ * @description
+ * # amdProfileForm
+ */
+angular.module('angularMaterialBoilerplateApp')
+  .component('amdProfileForm',  {
+    templateUrl: 'views/amd-profile-form.html',
+    controller: ['$log',
+      function amdProfileForm($log) {
+        var $ctrl = this;
+
+
+      }]
+  });

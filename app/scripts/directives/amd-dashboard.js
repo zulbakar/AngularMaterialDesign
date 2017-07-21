d9d422c7ee89394889fcbc6606d42e2b6b8bba50
azizulBakar <azizul_sb@yahoo.com>
2017-07-21 09:03:22 +0800

    upadte dashboard

--- app/scripts/directives/amd-dashboard.js
+++ app/scripts/directives/amd-dashboard.js
@@ -9,10 +9,27 @@
 angular.module('angularMaterialDesignApp')
   .component('amdDashboard', {
     templateUrl: 'views/amd-dashboard.html',
-    controller: ['$log',
-      function amdDashboard($log) {
+    controller: ['$log', '$scope',
+      function amdDashboard($log, $scope) {
         var $ctrl = this;
+        $ctrl.mdrowheightxs = "2:2";
 
+        /**WINDOW ON RESIZE */
+        window.onresize = function (event) {
+          $ctrl.applyOrientation();
+        }
 
+        $ctrl.applyOrientation = function () {
+          if (window.innerHeight > window.innerWidth) {
+            // you're in PORTRAIT mode
+            $ctrl.mdrowheightxs = "2:2";
+          } else {
+            // you're in LANDSCAPE mode
+            $ctrl.mdrowheightxs = "3:2";
+          }
+        }
+        $ctrl.applyOrientation();
+
+        /**END WINDOW ON RESIZE */
       }]
   });

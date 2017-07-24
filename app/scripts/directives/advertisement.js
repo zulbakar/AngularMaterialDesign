3afbacfbc87eb4aef255a0484d9deb7ad33d270e
azizulBakar <azizul_sb@yahoo.com>
2017-07-24 11:19:37 +0800

    add ads

---
+++ app/scripts/directives/advertisement.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name angularMaterialDesignApp.directive:advertisement
+ * @description
+ * # advertisement
+ */
+angular.module('angularMaterialDesignApp')
+  .component('advertisement', {
+    templateUrl: 'views/advertisement.html',
+    controller: ['$log',
+      function advertisement($log) {
+        var $ctrl = this;
+
+
+      }]
+  });
